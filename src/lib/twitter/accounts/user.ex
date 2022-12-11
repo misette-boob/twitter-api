e679070c35177fb8045caa4e8b5caada80c310eb
misette-boob <s.o.spiridonov@gmail.com>
2022-12-11 06:00:56 +0300

    Update validation length for password and name

--- src/lib/twitter/accounts/user.ex
+++ src/lib/twitter/accounts/user.ex
@@ -49,7 +49,7 @@ defmodule Twitter.Accounts.User do
   defp validate_password(changeset, opts) do
     changeset
     |> validate_required([:password])
-    |> validate_length(:password, min: 12, max: 72)
+    |> validate_length(:password, min: 8, max: 32)
     # |> validate_format(:password, ~r/[a-z]/, message: "at least one lower case character")
     # |> validate_format(:password, ~r/[A-Z]/, message: "at least one upper case character")
     # |> validate_format(:password, ~r/[!?@#$%^&*_0-9]/, message: "at least one digit or punctuation character")
@@ -74,8 +74,7 @@ defmodule Twitter.Accounts.User do
   defp validate_name(changeset) do
     changeset
     |> validate_required([:name])
-    |> validate_length(:name, max: 32)
-    |> validate_length(:name, min: 3)
+    |> validate_length(:name, min: 3, max: 32)
   end
 
   @doc """
