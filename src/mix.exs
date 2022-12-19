32b9b02186961444f068438e5cd122f93911f813
misette-boob <s.o.spiridonov@gmail.com>
2022-12-19 15:53:52 +0300

    seeds

--- src/mix.exs
+++ src/mix.exs
@@ -50,7 +50,8 @@ defmodule Twitter.MixProject do
       {:gettext, "~> 0.18"},
       {:jason, "~> 1.2"},
       {:plug_cowboy, "~> 2.5"},
-      {:guardian, "~> 2.0"}
+      {:guardian, "~> 2.0"},
+      {:faker, "~> 0.16", only: [:test, :dev]}
     ]
   end
 
